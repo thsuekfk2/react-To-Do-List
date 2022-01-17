def12e8bef93e1379bd76985d536c838adc8060a
Hyebin <thsuekfk2@naver.com>
2022-01-17 18:18:24 +0900

    React + Node.js(Express) 연동하기

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+
+function App() {
+  const callApi = async () => {
+    axios.get("/api").then((res) => console.log(res.data.test));
+  };
+  useEffect(() => {
+    callApi();
+  }, []);
+  return <div className="App">test</div>;
+}
+
+export default App;
