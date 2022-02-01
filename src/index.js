4fef4cc86bf939de37fc0dfe1f0354b3f14770a2
Hyebin <thsuekfk2@naver.com>
2022-02-02 01:24:35 +0900

    redux 설치

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
 
+const createStoreWithMiddleware = applyMiddleware;
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
