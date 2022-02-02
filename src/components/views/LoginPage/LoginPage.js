6db7229dd87dbb6ffdcdf9b3a00f532fead87926
Hyebin <thsuekfk2@naver.com>
2022-02-02 22:50:24 +0900

    Login page (with redux)

--- src/components/views/LoginPage/LoginPage.js
+++ src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,65 @@
-import React from "react";
+import { Axios } from "axios";
+import React, { useState } from "react";
+import TodoTemplateBlock from "../../TodoTemplate";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
+function LoginPage(props) {
+  const dispatch = useDispatch();
 
-function LoginPage() {
-  return <div>LoginPage</div>;
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    //preventDefault를 해주는 이유?
+    //안해줄 경우 누르면 리프레쉬가 된다. 리프레쉬를 막기 위해!
+
+    let body = {
+      email: Email,
+      password: Password,
+    };
+    //redux를 사용하여
+    dispatch(loginUser(body)).then((res) => {
+      if (res.payload.loginSuccess) {
+        props.history.push("/");
+      }
+    });
+  };
+  return (
+    <TodoTemplateBlock>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={onSubmitHandler}
+        >
+          <label>Email</label>
+          <input type="email" value={Email} onChange={onEmailHandler}></input>
+          <label>Password</label>
+          <input
+            type="password"
+            value={Password}
+            onChange={onPasswordHandler}
+          ></input>
+          <br />
+          <button>Login</button>
+        </form>
+      </div>
+    </TodoTemplateBlock>
+  );
 }
 
 export default LoginPage;
