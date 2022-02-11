1f2cdeec4c05ddceb016bdd3a9c8b9950a16afe0
Hyebin <thsuekfk2@naver.com>
2022-02-11 20:16:44 +0900

    Logout

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -6,10 +6,23 @@ function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((res) => console.log(res.data));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+    .then(res=>{
+      if (res.data.success) {
+        props.history.push("/login");
+      }else{
+        alert('로그아웃이 실패했습니다.')
+      }
+    })
+  }
+  
   return (
     <div>
       <TodoTemplate>
         <h2>시작</h2>
+        <button onClick={onClickHandler}>로그아웃</button>
       </TodoTemplate>
     </div>
   );
