7b286ae420ab6d92e9f51c0e4231f5d81a53a712
Hyebin <thsuekfk2@naver.com>
2022-02-18 21:29:32 +0900

    upload page 레이아웃 설정

--- src/components/views/UploadListPage/UploadListPage.js
+++ src/components/views/UploadListPage/UploadListPage.js
@@ -1,7 +1,32 @@
-import React from "react";
+import { React, useState } from "react";
 import Layout from "../../Layout/Layout";
 import "./UploadListPage.scss";
 function UploadListPage() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [period, setPeriod] = useState(1);
+  const [image, setImage] = useState([]);
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.currentTarget.value);
+  };
+  const descriptionChangeHandler = (event) => {
+    setDescription(event.currentTarget.value);
+  };
+  const priceChangeHandler = (event) => {
+    setPrice(event.currentTarget.value);
+  };
+  const periodChangeHandler = (event) => {
+    setPeriod(event.currentTarget.value);
+  };
+  const PeriodContents = [
+    { key: 1, value: "Day" },
+    { key: 2, value: "Week" },
+    { key: 3, value: "Month" },
+    { key: 4, value: "Year" },
+    { key: 5, value: "Bucket list" },
+  ];
   return (
     <div>
       <Layout>
@@ -13,17 +38,28 @@ function UploadListPage() {
           <div className="upload-input-form">
             <form style={{ display: "flex", flexDirection: "column" }}>
               <label>Your dream</label>
-              <input></input>
+              <input value={title} onChange={titleChangeHandler} />
 
               <label>Explanation</label>
-              <textarea></textarea>
+              <textarea
+                value={description}
+                onChange={descriptionChangeHandler}
+              ></textarea>
 
               <label>Price</label>
-              <input></input>
-              <label>select</label>
-              <select>
-                <option>1</option>
-                <option>2</option>
+              <input
+                type="number "
+                value={price}
+                onChange={priceChangeHandler}
+              ></input>
+
+              <label>Period</label>
+              <select onChange={periodChangeHandler} value={period}>
+                {PeriodContents.map((a) => (
+                  <option key={a.key} value={a.key}>
+                    {a.value}
+                  </option>
+                ))}
               </select>
               <button>Confirm</button>
             </form>
