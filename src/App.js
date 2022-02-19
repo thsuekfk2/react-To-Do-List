3c347cf2a526d4920f82b96c0749eea781aa31fa
Hyebin <thsuekfk2@naver.com>
2022-02-19 20:43:35 +0900

    Shop Page 제작 및 기본 레이아웃 적용

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Auth from "./hoc/auth";
 import Mypage from "./components/views/Mypage/Mypage";
 import CartPage from "./components/views/CartPage/CartPage";
 import UploadListPage from "./components/views/UploadListPage/UploadListPage.js";
+import ShopPage from "./components/views/ShopPage/ShopPage.js";
+
 const GlobalStyle = createGlobalStyle`
   body {
     margin:0;
@@ -29,6 +31,7 @@ function App() {
         <Route exact path="/mypage" component={Auth(Mypage, true)} />
         <Route exact path="/register" component={Auth(RegisterPage, null)} />
         <Route exact path="/cart" component={Auth(CartPage, null)} />
+        <Route exact path="/shop" component={Auth(ShopPage, null)} />
         <Route
           exact
           path="/todo/upload"
